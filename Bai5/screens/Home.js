f7595b9f85412b38c068205a45b8096edaf79db1
NguyenNgocVuong123 <112496039+NguyenNgocVuong123@users.noreply.github.com>
2022-10-17 11:00:22 +0700

    vuong
    cs

--- Bai5/screens/Home.js
+++ Bai5/screens/Home.js
@@ -1,21 +1,30 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import React, { useEffect, useState } from 'react';
-import { FlatList, ScrollView, StatusBar, Text, View, SafeAreaView } from 'react-native';
+import React, { useEffect, useState,Component  } from 'react';
+import { placeholder,FlatList, ScrollView, ImageBackground, StatusBar, Text, View, SafeAreaView,TextInput,Dimensions } from 'react-native';
 import DrinkItem from '../src/components/DrinkItem';
-import data from '../src/data/drinks.json';
+import Ips from "../src/input";
+import { Fontisto } from '@expo/vector-icons';
 import dataList from "../src/data/data";
 import styles from './styles';
 import Btns from "../src/btn";
 import axios from 'axios'
-export default function HomeScreen({ navigation }) {
+import Swiper from 'react-native-swiper';
+import Btntab from "../src/btntab";
+import Item from "../src/Iteam";
+function HomeScreen({ navigation }) {
   const [user, setuser] = useState(null);
   const [apidata, setApidata] = useState([]);
+  const [data1, setdata1] = useState([]);
+  let ScreenHeight = Dimensions.get("window").height;
+  let urlpro = `http://192.168.32.105:3000/products`;
+  let urlpro1 = `http://192.168.32.105:3000/products/1`;
+  const [data2, setdata2] = useState([]);
   const renderItem = ({ item, index }) => {
     return <DrinkItem item={item} index={index} navigation={navigation} />;
   };
   const logOut = async () => {
     const res = await axios.get(
-      `http://192.168.0.100:3000/user`
+      `http://192.168.32.105:3000/user`
     );
     navigation.reset({
       index: 0,
@@ -23,12 +32,27 @@ export default function HomeScreen({ navigation }) {
     });
   };
   const getapi = ()=>{
-    axios.get(`http://192.168.0.100:3000/products`).then((Response)=> {
-      setApidata(response.data);
+    axios.get(`http://192.168.32.105:3000/products`).then((Response)=> {
+      setApidata(Response.data);
     });
   };
 
-  useEffect(() => getapi(),[]);
+  useEffect(function () {
+    fetch(urlpro)
+      .then((e) => e.json())
+      .then((rep) => setApidata(rep))
+      .catch((err) => {
+        setApidata([]);
+      });
+  }, []);
+  useEffect(function () {
+    fetch(urlpro1)
+      .then((e) => e.json())
+      .then((rep) => setdata1(rep))
+      .catch((err) => {
+        setdata1([]);
+      });
+  }, []);
   // componentDidMount() {
   //   axios.get(`http://192.168.0.100:3000/products`)
   //     .then(res => {
@@ -62,22 +86,67 @@ export default function HomeScreen({ navigation }) {
   //     console.log(error);
   //   }
   // };
+  const image0 = { uri: "https://previews.123rf.com/images/pazhyna/pazhyna1610/pazhyna161000045/64061908-fast-food-seamless-pattern-in-hand-drawn-doodle-style-with-different-objects-on-fast-food-theme-all-.jpg" };
+  const image1 = { uri: "https://media.self.com/photos/622912847b959736301bfb91/2:1/w_1280,c_limit/GettyImages-1301412050.jpg" };
+  const image2 = { uri: "https://images.squarespace-cdn.com/content/v1/53b839afe4b07ea978436183/1608506169128-S6KYNEV61LEP5MS1UIH4/traditional-food-around-the-world-Travlinmad.jpg" };
+  const image3 = { uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5n06oW6oAiLlsj2KGNDntJwbBCBKs77CzJLcy_rrNKucMyVKwgDTd1WSE20zVE6vTTjs&usqp=CAU" };
   return (
-    <View>
-      {apidata.map((item)=> {
-        return(
-        //   <FlatList
-        //   data={item}
-        //   horizontal
-        //   showsHorizontalScrollIndicator={true}
-        //   keyExtractor={(item, index) => item + index}
-        //   renderItem={renderItem}
-        // />
-        <View><Text>{item.name}</Text></View>
-        )
-      })}
+    <ScrollView>
+  <ImageBackground source={image0} style={styles.bg}>
+      <View style={{marginTop:StatusBar.currentHeight,alignItems: 'center',
+    justifyContent: 'center',marginBottom:10}}><TextInput  style={{padding: 10, height: 40,width:'90%', borderColor: 'gray', borderWidth: 1,borderRadius: 10, fontSize:20,backgroundColor:'rgba(240,241,242,0.6)',  }} placeholder="Nấu món gì đây ta?"  ></TextInput></View>
+      <View style={{height:200,borderWidth:3, borderRadius: 20 }}><Swiper style={styles.wrapper} showsButtons autoplay={{delay: 2500}}>
+  <ImageBackground source={image1} style={styles.imgbgs}>
+  <View style={styles.slide1}>
+    <Text style={styles.text}>Thơm ngon</Text>
+  </View>
+  </ImageBackground>
+  <ImageBackground source={image2} style={styles.imgbgs}>
+  <View style={styles.slide2}>
+    <Text style={styles.text}>Mời bạn</Text>
+  </View>
+  </ImageBackground>
+  <ImageBackground source={image3} style={styles.imgbgs}>
+  <View style={styles.slide3}>
+    <Text style={styles.text}>Ăn ngay</Text>
+  </View>
+  </ImageBackground>
+</Swiper></View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.title}>Các Món Theo Lượt Yêu Thích</Text>
+        <FlatList
+          data={data1}
+          horizontal
+          showsHorizontalScrollIndicator={true}
+          numRow = {2}
+          keyExtractor={(item, index) => item + index}
+          renderItem={renderItem} />
+        
+        </View>
+    <View style={{height:70 , backgroundColor: 'red'}}>
+      <FlatList
+      data={apidata}
+      horizontal
+      showsHorizontalScrollIndicator={true}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => <Item name={item.ingredient} />}/>
     </View>
-    
+    <View style={styles.sectionContainer}>
+    {apidata === "" ? (
+      <Text style={styles.loadingText}>Loading...</Text>
+    ) : (
+      
+      <FlatList
+        data={apidata}
+        // renderItem={({ item }) => <Item name={item.name} />}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => item + index}
+        numColumns = {2}
+      />
+    )}
+  </View>
+  </ImageBackground>
+  </ScrollView>
     // <ScrollView 
     //   style={{
     //     backgroundColor: '#fff',
@@ -105,8 +174,8 @@ export default function HomeScreen({ navigation }) {
     //       </Text>
     //     </View>
     //   </View>
-    //   <View style={styles.sectionContainer}>
-    //     <Text style={styles.title}>Các Món Có Thể Bạn Sẽ Thích</Text>
+      // <View style={styles.sectionContainer}>
+      //   <Text style={styles.title}>Các Món Có Thể Bạn Sẽ Thích</Text>
       //   <FlatList
       //     data={apidata}
       //     horizontal
@@ -142,3 +211,4 @@ export default function HomeScreen({ navigation }) {
 
   );
 }
+export default HomeScreen;
--- Bai5/screens/styles.js
+++ Bai5/screens/styles.js
@@ -5,8 +5,51 @@ export default StyleSheet.create({
     marginTop:20
   },
   title: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: "bold",
     marginBottom: 12,
   },
+  bg: {
+    width: '100%',
+    height: '100%',
+  },
+
+  image1:{
+    borderWidth: 2,
+    borderRadius: 20
+  },
+
+  imgbgs: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  slide1: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0, 0.40)'
+    
+  },
+  slide2: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0, 0.40)'
+  },
+  slide3: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0, 0.40)'
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: '900'
+  }
 });
